5421b939321a38e4356007fb273023dd2741e0ff
adriano.souza <adriano.souza@zpesystems.com>
2023-02-23 19:16:45 -0300

    Add fortawesone library and setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider, DefaultTheme } from 'styled-components'
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import GlobalStyle from '../styles/globals'
+
+config.autoAddCss = false;
 
 const theme: DefaultTheme = {
   colors: {
