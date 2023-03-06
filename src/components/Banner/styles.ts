d6903708b5636b66362d34f68fcf9de44071ee95
adriano.souza <adriano.souza@zpesystems.com>
2023-03-06 20:39:57 -0300

    Resized banner container

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -6,4 +6,5 @@ export const BannerContainer = styled.section`
   ${Container};
   border-top: 3px solid ${({ theme }) => theme.colors.primary};
   border-bottom: 3px solid ${({ theme }) => theme.colors.primary};
+  padding-top: 4px;
 `
